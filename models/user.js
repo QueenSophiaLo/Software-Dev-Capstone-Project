b651e114f439c618d2602ec4fa414ca43b6d2dd2
Ashrab Risal <arisal@uncc.edu>
2025-09-17 11:51:38 -0400

    commit

--- models/user.js
+++ models/user.js
@@ -3,7 +3,9 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
     firstName: {type: String},
-    lastName: {type: String}
+    lastName: {type: String},
+    email: {type: String},
+    password: {type: String}
 })
 
 module.exports = mongoose.model('User', userSchema);
