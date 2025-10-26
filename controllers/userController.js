b85eb9518c57ff6dc291bbfbbfb2fbe5dca38a3f
Ashrab Risal <arisal@uncc.edu>
2025-10-25 20:34:00 -0400

    added in log in funcs will test soon

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,9 +9,29 @@ exports.login = (req, res, next)=>{
         if(user){
             user.comparePassword(password)
             .then(result =>{
+                if(result){
+                    req.session.user = user._id;
+                    req.session.save(() =>{
+                        res.redirect('/users/profile')
+                    })
+                }
+                else{
+                    console.log("wrong password")
+                    req.session.save(() =>{
+                        res.redirect('/users/login')
+                    })
+                }
+            })
+            .catch()
+        }
+        else{
+            console.log("wrong email")
+            req.session.save(() =>{
+                res.redirect('/users/login')
             })
         }
     })
+    .catch(err => next(err))
 };
 
 exports.signup = (req, res, next) =>{
