a0f0832cfafcc1c2cfc54bc49c34621ab45b9c97
Y-hp246 <klopez26@charlotte.edu>
2025-10-28 10:03:05 -0400

    changes
    minor changes

--- models/finance-data.js
+++ models/finance-data.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const financeData = new Schema ({
     userId: {type: Schema.Types.ObjectId, ref: 'User'},
     category: {type: String, required: [true, 'Category is required'], 
-        enum: ['Food', 'Rent', 'Transportation', 'Entertainment', 'Other']},
+    enum: ['Food', 'Rent', 'Transportation', 'Entertainment', 'Other']},
     amount: {type: Number, required: [true, 'Amount is required'], min: [0.01, 'minimum price is 0.01'], },
     date: {type: String, date: new Date(), required: [true, 'Date is required']},
     description: {type: String, required: [true, 'Description is required']}
